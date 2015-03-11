5f7fd4b58e456c754f5cca55421c17f7151307b8
Joseph Mingrone <jrm@ftfl.ca>
2015-03-11 17:46:34 -0300

    Don't store shell commands in variables

--- ml_bat.sh
+++ ml_bat.sh
@@ -1,15 +1,14 @@
 #!/bin/sh
 
-## You may need to customize the sysctl command below to use the variables that
-## show your system's battery state and life.
+## You may need to customize the sysctl command below to use variables that show
+## your system's battery state and remaining life.
 
-bat_cmd="/sbin/sysctl -n hw.acpi.battery.state hw.acpi.battery.life | tr '\n' ' '"
 interval=15
 stump_pid=`pgrep -a -n stumpwm`
 
 # while stumpwm is still running
 while kill -0 $stump_pid > /dev/null 2>&1; do
-    bat_info=`eval ${bat_cmd}`
-    echo ${bat_info}
+    echo $(/sbin/sysctl -n hw.acpi.battery.state hw.acpi.battery.life \
+		  | tr '\n' ' ')
     sleep ${interval}
 done
