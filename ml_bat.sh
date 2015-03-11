b2f5b6e1f6dab7922ae339ce95b560e083878b67
Joseph Mingrone <jrm@ftfl.ca>
2015-03-11 17:51:56 -0300

    Don't use backticks

--- ml_bat.sh
+++ ml_bat.sh
@@ -3,8 +3,8 @@
 ## You may need to customize the sysctl command below to use variables that show
 ## your system's battery state and remaining life.
 
-interval=15
-stump_pid=`pgrep -a -n stumpwm`
+interval=15 # customize this
+stump_pid=$(pgrep -a -n stumpwm)
 
 # while stumpwm is still running
 while kill -0 $stump_pid > /dev/null 2>&1; do
