f92085f6a4ee5261293ba3463bc7039f8e1e3853
Joseph Mingrone <jrm@ftfl.ca>
2015-09-13 23:30:36 -0300

    Cosmetic changes

--- ml_bat.sh
+++ ml_bat.sh
@@ -8,7 +8,8 @@ stump_pid=$(pgrep -a -n stumpwm)
 
 # while stumpwm is still running
 while kill -0 $stump_pid > /dev/null 2>&1; do
-    echo $(/sbin/sysctl -n hw.acpi.battery.state hw.acpi.battery.life \
+    echo $(/sbin/sysctl -n hw.acpi.battery.state \
+			hw.acpi.battery.life \
 		  | tr '\n' ' ')
     sleep ${interval}
 done
