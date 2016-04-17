00c72bca9c967b9ee44cf2849460adfbd4b9f158
Joseph Mingrone <jrm@ftfl.ca>
2016-04-17 03:25:07 -0300

    Add a newline when the battery status is printed

--- ml_bat.sh
+++ ml_bat.sh
@@ -8,6 +8,6 @@ stump_pid="$(pgrep -a -n stumpwm)"
 
 # while stumpwm is still running
 while kill -0 "$stump_pid" > /dev/null 2>&1; do
-    /sbin/sysctl -n hw.acpi.battery.state hw.acpi.battery.life | tr '\n' ' '
+    sysctl -n hw.acpi.battery.state hw.acpi.battery.life | paste -s -d ' ' -
     sleep "$interval"
 done
