bfc6d43677a0466033f2d0b1c82be3fcedc54eb8
Joseph Mingrone <jrm@ftfl.ca>
2016-04-17 02:09:50 -0300

    Clean up

--- ml_bat.sh
+++ ml_bat.sh
@@ -4,11 +4,10 @@
 ## your system's battery state and remaining life.
 
 interval=15 # customize this
-stump_pid=$(pgrep -a -n stumpwm)
+stump_pid="$(pgrep -a -n stumpwm)"
 
 # while stumpwm is still running
-while kill -0 $stump_pid > /dev/null 2>&1; do
-    echo $(/sbin/sysctl -n hw.acpi.battery.state hw.acpi.battery.life \
-		  | tr '\n' ' ')
-    sleep ${interval}
+while kill -0 "$stump_pid" > /dev/null 2>&1; do
+    /sbin/sysctl -n hw.acpi.battery.state hw.acpi.battery.life | tr '\n' ' '
+    sleep "$interval"
 done
